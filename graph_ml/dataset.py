2046cf92ae52331784d2f718653fea0127921677
davidhughhenrymack <david@sketchdeck.com>
2017-12-10 14:52:44 -0800

    recipies for data

--- graph_ml/dataset.py
+++ graph_ml/dataset.py
@@ -1,13 +1,20 @@
 
+import collections
+
 from graph_io import *
 
 class Dataset(object):
 
-	def __init__(self, data):
+	def __init__(self, data, x, y):
 		self.data = data
+		self.data_x = x
+		self.data_y = y
+
+		for r in self.data_x:
+			print(r)
 
 		self.input_shape = (1,2,3,4)
-		
+
 		self.train = {}
 		self.test = {}
 		self.validate = {}
@@ -15,21 +22,39 @@ class Dataset(object):
 
 	@staticmethod
 	def generate(params):
+		with SimpleNodeClient() as client:
+			query_params = QueryParams()
 
-		if params.experiment == 'test':
-			return {
-				train: {x: [1], y:[1]},
-				validate: {x: [1], y:[1]},
-				test: {x: [1], y:[1]}
+			Recipe = collections.namedtuple('Recipe', ['query', 'get_x', 'get_y'])
+
+			recipies = {
+				'movie': Recipe(
+						"MATCH p=(a)-[r:ACTS_IN]->(b)<-[q:RATED]-(c) RETURN a.name, q.stars LIMIT 100",
+						lambda row: (row.name),
+						lambda row: row.stars
+					),
+				
+				'simple': Recipe(
+						"""MATCH p=
+								(a:PERSON {isGolden:false}) 
+									-[:WROTE {isGolden:false}]-> 
+								(b:REVIEW {isGolden:false}) 
+									-[:OF {isGolden:false}]-> 
+								(c:PRODUCT {isGolden:false})
+							RETURN a.style_preference, c.style, b.score
+							LIMIT 1000
+						""",
+						lambda row: (row.style_preference, row.style), 
+						lambda row: row.score
+					)
 			}
 
-		elif params.experiment == 'simple':
-			with SimpleNodeClient() as client:
+			recipe = recipies[params.experiment]
+			data = client.execute_cypher(CypherQuery(recipe.query), query_params)
+			x = map(recipe.get_x, data)
+			y = map(recipe.get_y, data)
 
-				query = CypherQuery("MATCH p=()-[r:DIRECTED]->()<-[q:ACTS_IN]-() RETURN p LIMIT 1000")
-				query_params = QueryParams()
-
-				return Dataset(client.execute_cypher(query, query_params))
+			return Dataset(data, x, y)
 		
 
 
