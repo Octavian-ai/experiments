2ed5db8c61be21febfc347a6d9b53f3b7a08bc43
Andrew Jefferson <andrew.jefferson@neotechnology.com>
2017-12-10 21:59:55 +0100

    add config

---
+++ config/__init__.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+from .environment import Environment
+
+default_values = {
+    'neo4j_url': 'bolt://010e4dc7-staging.databases.neo4j.io',
+    'neo4j_user': 'readonly',
+    'neo4j_password': 'neo4j_movies_db!'
+}
+
+overrides = defaultdict(dict)
+overrides.update(**{
+    'andrew': {
+
+    },
+    'david': {
+
+    },
+    'floydhub': {
+
+    }
+})
+
+environment_box = Environment(None)
+
+
+def set_environment(environment_name):
+    environment_box.name = environment_name
+
+
+def get(config_variable_name):
+    return overrides[environment_box.name].get(config_variable_name, default_values[config_variable_name])
+
+
+class Config(object):
+    @property
+    def neo4j_url(self):
+        return get('neo4j_url')
+
+    @property
+    def neo4j_user(self):
+        return get('neo4j_user')
+
+    @property
+    def neo4j_password(self):
+        return get('neo4j_password')
+
+
+config: Config = Config()
