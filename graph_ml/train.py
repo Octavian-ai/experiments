1a797f718617b3887889388e249350271d1871fa
davidhughhenrymack <david@sketchdeck.com>
2017-12-10 20:38:23 -0800

    nice early stopping

--- graph_ml/train.py
+++ graph_ml/train.py
@@ -1,9 +1,35 @@
 
 import keras
 import numpy as np
+from keras.callbacks import Callback
 
 from .model import Model
 from .dataset import Dataset
+
+class StopEarlyIfAbove(Callback):
+	def __init__(self, monitor='val_acc', value=0.99, verbose=0):
+		super(Callback, self).__init__()
+		self.monitor = monitor
+		self.value = value
+		self.verbose = verbose
+		self.stopped_epoch = 0
+
+	def on_epoch_end(self, epoch, logs={}):
+		current = logs.get(self.monitor)
+		if current is None:
+			print("Early stopping requires %s available!" % self.monitor)
+			exit()
+
+		if current > self.value:
+			self.stopped_epoch = epoch
+			self.model.stop_training = True
+
+	def on_train_end(self, logs=None):
+		if self.stopped_epoch > 0 and self.verbose > 0:
+			print("Epoch {}: early stopping {} > {}".format(self.stopped_epoch+1, self.monitor, self.value))
+
+
+
 
 class Train(object):
 
@@ -15,6 +41,10 @@ class Train(object):
 
 		dataset = Dataset.lazy_generate(params)
 		model = Model.generate(params, dataset)
+
+		callbacks = [
+			StopEarlyIfAbove(verbose=params.verbose)
+		]
 		
 		model.fit(dataset.train.x, dataset.train.y,
 			batch_size=params.batch_size,
@@ -22,6 +52,7 @@ class Train(object):
 			verbose=params.verbose,
 			validation_split=0.1,
 			shuffle=True,
+			callbacks=callbacks
 		)
 
 		score = model.evaluate(dataset.test.x, dataset.test.y, verbose=0)
@@ -31,3 +62,5 @@ class Train(object):
 				print("Layer weights", layer.get_weights())
 
 		return score
+
+
