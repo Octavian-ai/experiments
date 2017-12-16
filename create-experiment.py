3bc2dc828b08716eea54781bfb7516a5f9ba44c3
davidhughhenrymack <david@sketchdeck.com>
2017-12-16 11:49:23 -0800

    create-experiment.py for #4

--- create-experiment.py
+++ create-experiment.py
@@ -11,11 +11,13 @@ if __name__ == '__main__':
 
 		# Just give me a fucking switch statement you imperative piece of shit
 		if FLAGS.experiment == '1':
-
 			# Let's move these to a dict for my factory
 			from data_sets.synthetic_review_prediction.experiment_1 import run
 		elif FLAGS.experiment == '2':
 			from data_sets.synthetic_review_prediction.experiment_2 import run
 		elif FLAGS.experiment == '3':
 			from data_sets.synthetic_review_prediction.experiment_3 import run
+		elif FLAGS.experiment == '4':
+			from data_sets.synthetic_review_prediction.experiment_4 import run
+			
 		run(client)
